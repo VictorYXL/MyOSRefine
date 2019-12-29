62c735aca8b2eb4fa72a6538d4475e10c3bff299
VictorYXL <hllya10@163.com>
2019-12-29 19:01:44 +0800

    First OS

--- build.cmd
+++ build.cmd
@@ -1,1 +1,7 @@
-nasm IPL.nsm -o MyOS.img
+echo off
+if not exist obj (
+    mkdir obj
+)
+nasm IPL.nsm -o obj\IPL.bin
+nasm FirstOS.nsm -o obj\FirstOS.bin
+tools\edimg.exe imgin:tools\fdimg0at.tek wbinimg src:obj\IPL.bin len:512 from:0 to:0 copy from:obj\FirstOS.bin to:@: imgout:MyOS1.img
